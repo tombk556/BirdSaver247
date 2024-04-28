a361067fae5c2c749b63190c74ff510fb62026aa
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-04-28 12:21:22 +0200

    added ip pic fetching

--- app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/IpAddressView.java
+++ app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/IpAddressView.java
@@ -1,20 +1,53 @@
 package htwd.s224.gruppe1.mnbirdsaver;
 
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.InputStream;
+
 public class IpAddressView extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ip_address_view);
+        imageView = findViewById(R.id.imageView3);
     }
 
-    public void submitIpAddress(View view){
-        // submit
+    public void submitIpAddress(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String ipAddress = editText.getText().toString();
+        new DownloadImageTask().execute("http://" + ipAddress + "/take_picture");  // Adjust the URL path as needed
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String urlDisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urlDisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
     }
 }
+
