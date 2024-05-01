4bb225ac38119ae8c03996e0fafa4b0d791d2ffa
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-05-01 17:54:08 +0200

    Download Image each 2 seconds

--- app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/IpAddressView.java
+++ app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/IpAddressView.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -16,18 +17,34 @@ import java.io.InputStream;
 public class IpAddressView extends AppCompatActivity {
 
     private ImageView imageView;
+    private EditText editText;
+    private Handler handler = new Handler();
+    private boolean isDownloading = false;
+    private Runnable imageDownloader = new Runnable() {
+        @Override
+        public void run() {
+            String ipAddress = editText.getText().toString();
+            new DownloadImageTask().execute("http://" + ipAddress + "/take_picture");
+            handler.postDelayed(this, 2000); // schedule next download in 2 seconds
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ip_address_view);
         imageView = findViewById(R.id.imageView3);
+        editText = findViewById(R.id.editText);
     }
 
     public void submitIpAddress(View view) {
-        EditText editText = findViewById(R.id.editText);
-        String ipAddress = editText.getText().toString();
-        new DownloadImageTask().execute("http://" + ipAddress + "/take_picture");  // Adjust the URL path as needed
+        if (!isDownloading) {
+            isDownloading = true;
+            handler.post(imageDownloader); // start downloading
+        } else {
+            isDownloading = false;
+            handler.removeCallbacks(imageDownloader); // stop downloading
+        }
     }
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
@@ -50,4 +67,3 @@ public class IpAddressView extends AppCompatActivity {
         }
     }
 }
-
