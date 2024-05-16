fc4e6694c27dc8a5159a98dec7673e388db96676
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-05-16 13:47:08 +0200

    added red dot to fetched pic

--- app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/Home.java
+++ app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/Home.java
@@ -33,15 +33,22 @@ import com.google.android.gms.location.Priority;
 import java.io.InputStream;
 import java.util.Calendar;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.Log;
+
+import java.util.Random;
 
 public class Home extends AppCompatActivity {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private ImageView imageView;
     private String ip_address;
-    private Button toggleButton;  // Reference to the button
+    private Button toggleButton;
     private Handler handler = new Handler();
 
-    private static final int PERMISSION_FINE_LOCATION = 99; // irgendwelche Identifikationsnummer
+    private static final int PERMISSION_FINE_LOCATION = 99;
 
     TextView tv_gps, tv_timestamp;
     LocationRequest locationRequest;
@@ -62,81 +69,67 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_view);
 
-        requestLocationPermission();  // necessary for GPS
+        requestLocationPermission();
 
         imageView = findViewById(R.id.view);
         tv_gps = findViewById(R.id.gpsValue);
         tv_timestamp = findViewById(R.id.dateValue);
 
-        // GPS (START) -----------------------------------------------------------------------------
-
-        // Initialising FusedLocationProviderClient
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        // Configuration of LocationRequest
         locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 2000)
-                .setWaitForAccurateLocation(false)   // true???
+                .setWaitForAccurateLocation(false)
                 .setMinUpdateIntervalMillis(2000)
                 .setMaxUpdateDelayMillis(4000)
                 .build();
 
-        // Create the Location Callback
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
                 for (Location location : locationResult.getLocations()) {
-                    // GUI mit Standortdaten aktualisieren
                     updateUI_values(location);
-                    // TO-DO: Bild der Kamera fetchen..., Pixel Detector und anschließend Datenabspeichern...
                 }
             }
         };
 
-
-        // GPS (End) -------------------------------------------------------------------------------
-
-
         try {
-            // if user put in an ip address in ip_address_view, the input is fetched here,
-            // if not the ip_address is undefined and the app will navigate to ip_address_view
             Intent intent = getIntent();
             ip_address = intent.getStringExtra("IPADDRESS");
 
             Log.d("CREATION", ip_address);
 
-            ip_address.length(); // make sure exception is caused if no ip address is available
+            if (ip_address == null || ip_address.isEmpty()) {
+                throw new NullPointerException("IP address is not provided");
+            }
 
             toggleButton = findViewById(R.id.submitButton);
             toggleButton.setText("Start");
 
-        } catch(NullPointerException e){
-            // navigate to ip address view if no ip address is available
+        } catch (NullPointerException e) {
             Intent intent = new Intent(this, IpAddressActivity.class);
             startActivity(intent);
         }
     }
-    // GPS -----------------------------------------------------------------------------------------
-    private void updateUI_values(Location location) {
-        System.out.println("New Coordiantes");
-        String gps_coordinates = convertToDMS(location.getLatitude())
-                +"\n"+ convertToDMS(location.getLongitude());
 
+    private void updateUI_values(Location location) {
+        System.out.println("New Coordinates");
+        String gps_coordinates = convertToDMS(location.getLatitude()) + "\n" + convertToDMS(location.getLongitude());
         tv_gps.setText(gps_coordinates);
         tv_timestamp.setText(Calendar.getInstance().getTime().toString());
     }
+
     @SuppressLint("DefaultLocale")
     private String convertToDMS(double decimalDegree) {
         int degree = (int) decimalDegree;
         double tempMinutes = (decimalDegree - degree) * 60;
         int minutes = (int) tempMinutes;
         double seconds = (tempMinutes - minutes) * 60;
-
         return String.format("%d° %d' %.6f\"", degree, minutes, seconds);
     }
 
     private void startLocationUpdates() {
         tv_gps.setText("Start");
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
         } else {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
@@ -147,13 +140,10 @@ public class Home extends AppCompatActivity {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
 
-
-    // Permission ----------------------------
     private void requestLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         } else {
-            // Berechtigung bereits erteilt, eventuell direkt zur nächsten Activity wechseln
             Toast.makeText(this, "Standortberechtigung bereits erteilt.", Toast.LENGTH_LONG).show();
         }
     }
@@ -163,29 +153,25 @@ public class Home extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Berechtigung wurde erteilt
                 Toast.makeText(this, "Standortberechtigung erteilt.", Toast.LENGTH_LONG).show();
             } else {
-                // Berechtigung wurde verweigert
                 Toast.makeText(this, "Standortberechtigung wurden verweigert.", Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    // GPS END -------------------------------------------------------------------------------------
+    public void startButtonClicked(View view) {
+        Log.d("CameraView", "startButtonClicked() wurde aufgerufen");
 
-    public void startButtonClicked(View view){
-        Log.d("CameraView", "startButtonClicked() wurde aufgerufen"); // Log-Nachricht hinzufügen
-
-        if (!isDownloading) {      // Start-Button
+        if (!isDownloading) {
             isDownloading = true;
             toggleButton.setText("Stop");
-            handler.post(imageDownloader); // start downloading
+            handler.post(imageDownloader);
             startLocationUpdates();
-        } else {                   // Stop-Button
+        } else {
             isDownloading = false;
             toggleButton.setText("Start");
-            handler.removeCallbacks(imageDownloader); // stop downloading
+            handler.removeCallbacks(imageDownloader);
             stopLocationUpdates();
         }
     }
@@ -205,25 +191,41 @@ public class Home extends AppCompatActivity {
             return mIcon11;
         }
 
+        @Override
         protected void onPostExecute(Bitmap result) {
-            imageView.setImageBitmap(result);
+            if (result != null) {
+                result = addRandomDot(result);
+                imageView.setImageBitmap(result);
+            }
+        }
+
+        private Bitmap addRandomDot(Bitmap bitmap) {
+            Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            Canvas canvas = new Canvas(mutableBitmap);
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);  // Set the dot color
+            paint.setStyle(Paint.Style.FILL);
+
+            Random random = new Random();
+            int x = random.nextInt(bitmap.getWidth());
+            int y = random.nextInt(bitmap.getHeight());
+
+            canvas.drawCircle(x, y, 10, paint);  // Draw a dot with radius 10
+
+            return mutableBitmap;
         }
     }
 
-    public void navigateToGPS(View view){
+    public void navigateToGPS(View view) {
         Intent intent = new Intent(this, GPSActivity.class);
         startActivity(intent);
     }
 
-    // use home button to navigate to camera view - here it goes back to itself
-    // TODO replace hardcoded ip address / disable Button
-    public void navigateToHome(View view){
+    public void navigateToHome(View view) {
         Log.d("CREATION", "nav");
 
-        // navigate to ip home view
         Intent intent = new Intent(this, Home.class);
-        intent.putExtra("IPADDRESS", "141.56.131.15"); // make sure value for ip address can be used in Home / camera_view
+        intent.putExtra("IPADDRESS", "141.56.131.15");
         startActivity(intent);
     }
-
 }
