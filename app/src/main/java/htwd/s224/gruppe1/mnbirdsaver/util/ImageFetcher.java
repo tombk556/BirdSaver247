d5723970dabd71699f95f0bfb9afb161b3ff4af2
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-06-20 09:52:05 +0200

    Toms documenmtation

--- app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/util/ImageFetcher.java
+++ app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/util/ImageFetcher.java
@@ -1,3 +1,7 @@
+/**
+ * Class for fetching images from a specified IP address and detecting red pixels within the images.
+ * Optionally, it can add an arc dot to the images for simulation purposes.
+ */
 package htwd.s224.gruppe1.mnbirdsaver.util;
 
 import android.graphics.Bitmap;
@@ -23,10 +27,27 @@ public class ImageFetcher {
     private float currentAngle = 0.0f; // Start angle in degrees
     private static final float ANGLE_INCREMENT = 10.0f; // Angle increment in degrees
 
+    /**
+     * Listener interface for red pixel coordinates detection.
+     */
     public interface RedPixelCoordinatesListener {
+        /**
+         * Called when red pixel coordinates are detected.
+         *
+         * @param x The x-coordinate of the red pixel.
+         * @param y The y-coordinate of the red pixel.
+         */
         void onRedPixelCoordinatesDetected(int x, int y);
     }
 
+    /**
+     * Constructs an ImageFetcher with the specified IP address, ImageView, listener, and arc dot inclusion flag.
+     *
+     * @param ip_address The IP address from which to fetch the image.
+     * @param imageView The ImageView in which to display the fetched image.
+     * @param listener The listener for red pixel coordinates detection.
+     * @param includeArcDot A flag indicating whether to include an arc dot in the image.
+     */
     public ImageFetcher(String ip_address, ImageView imageView, RedPixelCoordinatesListener listener, boolean includeArcDot) {
         this.ip_address = ip_address;
         this.imageView = imageView;
@@ -35,19 +56,32 @@ public class ImageFetcher {
         this.redPixelCoordinates = new ArrayList<>();
     }
 
+    /**
+     * Starts the process of fetching an image from the specified IP address.
+     */
     public void startFetching() {
         new DownloadImageTask().execute("http://" + ip_address + "/take_picture");
     }
 
-
+    /**
+     * Increments the arc dot's angle.
+     */
     public void increment_arc_dot() {
         currentAngle += ANGLE_INCREMENT;
     }
 
+    /**
+     * Returns the list of red pixel coordinates detected in the image.
+     *
+     * @return A list of red pixel coordinates.
+     */
     public List<PixelDetector.Coordinate> getRedPixelCoordinates() {
         return redPixelCoordinates;
     }
 
+    /**
+     * AsyncTask for downloading an image from a URL in the background.
+     */
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         @Override
         protected Bitmap doInBackground(String... urls) {
@@ -85,6 +119,12 @@ public class ImageFetcher {
             }
         }
 
+        /**
+         * Adds an arc dot to the specified Bitmap image.
+         *
+         * @param bitmap The Bitmap image to which the arc dot will be added.
+         * @return The Bitmap image with the arc dot added.
+         */
         private Bitmap addArcDot(Bitmap bitmap) {
             int imageWidth = bitmap.getWidth();
             int imageHeight = bitmap.getHeight();
@@ -105,9 +145,6 @@ public class ImageFetcher {
             int y = (int) (ARC_CENTER_Y + ARC_RADIUS * Math.sin(radians));
 
             canvas.drawCircle(x, y, 10, paint);  // Draw a dot with radius 10
-
-            // Increment the angle for the next dot
-            //currentAngle += ANGLE_INCREMENT;
 
             return mutableBitmap;
         }
--- app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/util/PixelDetector.java
+++ app/src/main/java/htwd/s224/gruppe1/mnbirdsaver/util/PixelDetector.java
@@ -1,3 +1,6 @@
+/**
+ * Utility class for detecting red pixels in an image.
+ */
 package htwd.s224.gruppe1.mnbirdsaver.util;
 
 import android.graphics.Bitmap;
@@ -6,29 +9,64 @@ import java.util.List;
 
 public class PixelDetector {
 
+    /**
+     * Represents a coordinate with x and y values.
+     */
     static class Coordinate {
         int x;
         int y;
 
-        public Coordinate(int x, int y) {
+        /**
+         * Constructs a Coordinate with the specified x and y values.
+         *
+         * @param x The x-coordinate.
+         * @param y The y-coordinate.
+         */
+        private Coordinate(int x, int y) {
             this.x = x;
             this.y = y;
         }
 
+        /**
+         * Returns a string representation of the Coordinate in JSON format.
+         *
+         * @return A string representing the coordinate in JSON format.
+         */
         @Override
         public String toString() {
             return "{\"x\": " + this.x + ", \"y\": " + this.y + "}";
         }
 
-        public int getX(){
+        /**
+         * Returns the x-coordinate.
+         *
+         * @return The x-coordinate.
+         */
+        public int getX() {
             return this.x;
         }
 
-        public int getY(){
-            return  this.y;
+        /**
+         * Returns the y-coordinate.
+         *
+         * @return The y-coordinate.
+         */
+        public int getY() {
+            return this.y;
         }
     }
 
+    /**
+     * Detects red pixels in the given Bitmap image.
+     *
+     * Red pixels are defined as those with red values greater than 200,
+     * green values less than 50, and blue values less than 50.
+     *
+     * @param image The Bitmap image to be analyzed.
+     * @return A list of Coordinates of the red pixels found in the image. If red pixels are found,
+     *         the list will contain a single Coordinate representing the center of the red pixels.
+     *         If no red pixels are found, the list will be empty.
+     */
     public static List<Coordinate> isPixelRed(Bitmap image) {
         List<Coordinate> redPixels = new ArrayList<>();
         int width = image.getWidth();
@@ -40,7 +78,7 @@ public class PixelDetector {
                 int red = (pixel >> 16) & 0xff;
                 int green = (pixel >> 8) & 0xff;
                 int blue = pixel & 0xff;
-
+                // Adjust the color settings for pixel recognition, here: red
                 if (red > 200 && green < 50 && blue < 50) {
                     redPixels.add(new Coordinate(x, y));
                 }
